6498e42f4204a275b4c231ad1c03ed728af69a8f
Athirson Silva <athirsonarceus@gmail.com>
2022-09-08 09:44:52 -0300

    schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 /* import { AppController } from './app.controller';
 import { AppService } from './app.service'; */
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://AthirsonSilva:root@nestjs-crud-api.d8ehbpn.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
